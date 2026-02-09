c379742a151a67f8a7ddb7e005c5a860b0544a57
JuanObandoDev <juanobando.developer@gmail.com>
2026-02-09 11:39:39 -0500

    feat: Advanced formatting modifiers in FStrings for numbers and dates

--- Clase3/f-strings.py
+++ Clase3/f-strings.py
@@ -1,21 +1,53 @@
 name = "Ana"
 born_year = 1990
 text = f"Hola, {name}!"
-print(text)
+# print(text)
 
 text_format = "Hola, {}!".format(name)
-print(text_format)
+# print(text_format)
 
 text_suma = f"El resultado de 2 + 2 es {2 + 2}."
-print(text_suma)
+# print(text_suma)
 
 text_calc = f"Hola, {name}. Tu edad es: {2026 - born_year} años."
-print(text_calc)
+# print(text_calc)
 
 
 text_func = f"HOLA {name.upper()}!"
-print(text_func)
+# print(text_func)
 
 edad = 30
 text_if = f"Hola {name}, tienes {'más de 30 años' if edad > 30 else '30 años o menos'}."
-print(text_if)
+# print(text_if)
+
+bank_balance = 1200000000
+text = f"Tu saldo bancario es: {bank_balance:,}."
+print(text)
+
+stock_price = 1.405
+text_stock = f"El valor del stock es: {stock_price:.1f}."
+print(text_stock)
+
+user_id = 1
+text_id = f"El ID del usuario es: {user_id:04d}."
+print(text_id)
+
+product = "laptop"
+price = 1000
+text_product = f"Producto {product:>10} | Precio: ${price:.2f}"
+print(text_product)
+
+from datetime import datetime
+
+date = datetime(2026, 12, 5, 10, 10)
+text = f"La fecha completa es: {date: %A %d de %B de %Y a las %I:%M %p}."
+print(text)
+
+# Formatear porcentajes y numeros en notación científica - Format percentages and numbers in scientific notation
+percentage = 0.1234
+text_percentage = f"El porcentaje es: {percentage:.2%}."
+print(text_percentage)
+
+number = 123456789
+text_scientific = f"El número en notación científica es: {number:.2e}."
+print(text_scientific)
